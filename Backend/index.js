2141acd44c9c91401cc44919ba9a9562b89dcf8f
Induni11 <indunisankalpani6@gmail.com>
2026-02-19 14:50:08 +0530

    Citizen Engagement & Feedback module

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./Routes/userRoutes.js";
 
+
+
 dotenv.config();
 
 const app = express();
@@ -14,6 +16,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/users", userRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
