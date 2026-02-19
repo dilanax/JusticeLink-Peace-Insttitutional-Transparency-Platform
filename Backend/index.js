91952dbdfc7f6eb0cce7c159f6cf7f4dca400839
dilan <dilanavishka183@gmail.com>
2026-02-19 22:41:29 +0530

    Revert "Merge pull request #3 from dilanax/citizen-feedback"
    This reverts commit c5adb9247a30bfa338073e2f12071cb6f5c404be, reversing
    changes made to 6515ca7adbdae0b1992b84422dee03fb24c29aa8.

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./Routes/userRoutes.js";
 
-
-
 dotenv.config();
 
 const app = express();
@@ -16,7 +14,6 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/users", userRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
