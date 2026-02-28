1e57d6037a3e13d768d219a81f4cb3f53a1a7365
Mona lisa <mona@gmail.com>
2026-02-28 08:41:30 +0530

    fnal

--- Backend/Routes/politicianRoutes.js
+++ Backend/Routes/politicianRoutes.js
@@ -12,13 +12,13 @@ import { authorizeRoles } from "../Middleware/roleMiddleware.js";
 
 const router = express.Router();
 
-// 🔐 ADMIN ONLY
+// PUBLIC
+router.get("/", getPoliticians);
+router.get("/:id", getPoliticianById);
+
+// ADMIN ONLY
 router.post("/", protect, authorizeRoles("admin"), createPolitician);
 router.put("/:id", protect, authorizeRoles("admin"), updatePolitician);
 router.delete("/:id", protect, authorizeRoles("admin"), deletePolitician);
-
-// 🌍 Public
-router.get("/", getPoliticians);
-router.get("/:id", getPoliticianById);
 
 export default router;
