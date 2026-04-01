1e9fbb8dbebf0bf177086f7f864554dbf47da60e
dilan <dilanavishka183@gmail.com>
2026-04-01 09:44:43 +0530

    ❤️

--- Backend/index.js
+++ Backend/index.js
@@ -20,6 +20,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:3000",
   "http://localhost:5173",
+  "http://localhost:5174",
   "http://localhost:5175",
 ].filter(Boolean);
 
